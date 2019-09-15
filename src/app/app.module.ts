053f39b3ccac37fe66544cd9563008bb1053a7bd
Lukasz <karolina.wojak@yahoo.com>
2019-09-15 17:38:48 +0200

    Textarea for entries and event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryCreateComponent } from './entries/entry-create/entry-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryCreateComponent
   ],
   imports: [
     BrowserModule,
--- src/app/entries/entry-create/entry-create.component.ts
+++ src/app/entries/entry-create/entry-create.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component ({
+  selector: 'app-entry-create',
   templateUrl: './entry-create.component.html'
 })
 
 export class EntryCreateComponent {
-
+  onAddEntry() {
+    alert('Entry added');
+  }
 }
