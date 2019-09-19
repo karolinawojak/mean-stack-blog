3d2d92e89f485b03bb44f61aabb09c9008e3e110
Lukasz <karolina.wojak@yahoo.com>
2019-09-19 12:52:19 +0200

    Styling

--- src/app/entries/entry-create/entry-create.component.ts
+++ src/app/entries/entry-create/entry-create.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'app-entry-create',
-  templateUrl: './entry-create.component.html'
+  templateUrl: './entry-create.component.html',
+  styleUrls: ['./entry-create.component.scss']
 })
 
 export class EntryCreateComponent {
