140126f6896e5a5337e125eeebdfc1e3da935906
Lukasz <karolina.wojak@yahoo.com>
2019-09-20 20:10:12 +0200

    Structure and styling

--- src/app/entries/entries-list/entries-list.component.ts
+++ src/app/entries/entries-list/entries-list.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component ({
   selector: 'app-entries-list',
-  templateUrl: './entries-list.component.html'
+  templateUrl: './entries-list.component.html',
+  styleUrls: ['./entries-list.component.scss']
 })
 
 export class EntriesListComponent {
