216b89a739582bb8bf58ad3118fab73e23f0be08
Lukasz <karolina.wojak@yahoo.com>
2019-09-24 12:22:18 +0200

    Adding entries

--- src/app/entries/entry.service.ts
+++ src/app/entries/entry.service.ts
@@ -7,4 +7,11 @@ export class EntryService {
     // a copy of entries array
     return [...this.entries];
   }
+
+  addEntry(title: string, content: string) {
+    // property shorthand instead of {title: title, content: content} - ES6
+    const entry: Entry = {title, content};
+    this.entries.push(entry);
+
+  }
 }
