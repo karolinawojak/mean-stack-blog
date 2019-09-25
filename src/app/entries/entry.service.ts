a42f163e8441036d2c9d1591ee9f0d99e69f520f
Lukasz <karolina.wojak@yahoo.com>
2019-09-25 08:21:33 +0200

    Added RxJS to the service

--- src/app/entries/entry.service.ts
+++ src/app/entries/entry.service.ts
@@ -1,19 +1,26 @@
 import { Entry } from './entry.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EntryService {
   private entries: Entry[] = [];
+  private entriesUpdate = new Subject<Entry[]>();
 
   getEntries() {
     // a copy of entries array
     return [...this.entries];
   }
 
+  getEntriesListener() {
+    return this.entriesUpdate.asObservable();
+  }
+
   addEntry(title: string, content: string) {
     // property shorthand instead of {title: title, content: content} - ES6
     const entry: Entry = {title, content};
     this.entries.push(entry);
+    this.entriesUpdate.next([...this.entries]);
 
   }
 }
