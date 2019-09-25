fdbfd8214191db85d6cc13b94eadf00c75e2b349
Lukasz <karolina.wojak@yahoo.com>
2019-09-25 14:09:31 +0200

    Setup for fetching entries

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((request, response, next) => {
-  response.send('hello');
+app.use('/entries', (request, response, next) => {
+  const entries = [
+    {
+      id: '1',
+      title: 'First title',
+      content: 'First content'
+    },
+    {
+      id: '2',
+      title: 'Second title',
+      content: 'Second content'
+      },
+  ];
+
+  response.status(200).json({
+    message: 'Posts fetched succesfully',
+    entries: entries
+  });
 })
 
 module.exports = app;
