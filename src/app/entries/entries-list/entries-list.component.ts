3e7d7c35f44749eb22fb8f583b25b6729d1b0ff7
Lukasz <karolina.wojak@yahoo.com>
2019-09-25 08:49:15 +0200

    Listening to an observable in entries-list

--- src/app/entries/entries-list/entries-list.component.ts
+++ src/app/entries/entries-list/entries-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Entry } from '../entry.model';
 import { EntryService } from '../entry.service';
 
@@ -8,13 +9,22 @@ import { EntryService } from '../entry.service';
   styleUrls: ['./entries-list.component.scss']
 })
 
-export class EntriesListComponent implements OnInit {
+export class EntriesListComponent implements OnInit, OnDestroy {
   entriesList: Entry[] = [];
+  private entriesSubscription: Subscription;
 
   // public instead of creating a property to store an incoming value in
   constructor(public entryService: EntryService) {}
 
   ngOnInit() {
     this.entriesList = this.entryService.getEntries();
+    this.entriesSubscription = this.entryService.getEntriesListener()
+      .subscribe((entriesListReceived: Entry[]) => {
+        this.entriesList = entriesListReceived;
+      });
+  }
+
+  ngOnDestroy() {
+    this.entriesSubscription.unsubscribe();
   }
 }
