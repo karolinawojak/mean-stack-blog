cf64d9477a03f5665eb245050936f8c6afd4457f
Lukasz <karolina.wojak@yahoo.com>
2019-10-13 22:40:33 +0200

    Allowing certain methods

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ app.use((request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers',
   'Origin, X-Requested-With, Content-Type, Accept');
+  response.setHeader('Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 })
 
